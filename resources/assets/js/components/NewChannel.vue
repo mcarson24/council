fd7cb351f63f5dc9cac7d7f2fbb3023f34893ed4
Matthew Carson <matthew24carson@gmail.com>
2018-02-17 16:14:46 -0500

    Create channel with ajax request, display errors.

--- resources/assets/js/components/NewChannel.vue
+++ resources/assets/js/components/NewChannel.vue
@@ -8,20 +8,32 @@
         	<div class="p-6 py-8">
 				<div class="flex-1 px-4 mb-6">
 				    <label for="name" class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Name</label>
-				    <input type="text" class="w-full p-2 leading-normal" id="name" name="name" required>
+				    <input v-model="name" type="text" 
+				    	   class="w-full p-2 leading-normal" 
+				    	   :class="{'border-red': hasError('name')}" 
+				    	   id="name" name="name"
+				    	   >
+		    	   	<span v-if="hasError('name')" v-text="this.errors['name'][0]"></span>
 				</div>
 
 				<div class="mb-6">
 		        	<div class="flex-1 px-4">
 					    <label for="description" class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Description</label>
-					    <input type="text" class="w-full p-2 leading-normal" id="description" name="description" required>
+					    <input v-model="description" type="text" 
+					    	   class="w-full p-2 leading-normal" 
+					    	   :class="{'border-red': hasError('description')}" 
+					    	   id="description" name="description"
+			    	    	   >
+	    	    	    <span v-if="hasError('description')" v-text="this.errors['description'][0]"></span>
 		        	</div>
 				</div>
 				
 	        	<div class="flex-1 px-4 mb-6">
 				    <label for="archived" class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Status</label>
 
-				    <select name="archived" id="archived" class="block appearance-none w-full bg-white rounded-none border border-grey-light text-grey-darker py-2 px-4 leading-normal pr-8">
+				    <select v-model="archived" name="archived" id="archived" 
+				    		class="block appearance-none w-full bg-white rounded-none border border-grey-light text-grey-darker py-2 px-4 leading-normal pr-8"
+							>
 				        <option value="0">Active</option>
 				        <option value="1">Archived</option>
 				    </select>
@@ -38,5 +50,37 @@
 
 <script>
 	export default {
+		data() {
+			return {
+				name: '',
+				description: '',
+				archived: false,
+				errors: []
+			}
+		},
+		methods: {
+			createChannel() {
+				console.log('Will post to: ' + location.pathname +  ` with ${this.name}, ${this.description}, and ${this.archived}`);
+				axios.post(location.pathname, {
+					name: this.name,
+					description: this.description
+				}).catch(error => {
+					this.errors = error.response.data.errors;
+					console.log(this.errors);
+					flash(error.response.data.message, 'danger');
+				}).then(({ data }) => {
+                    this.name = "";
+                    this.description = "";
+
+                    flash("Channel has been successfully created!");
+
+                    // TODO: Update channel list with newly created channel.
+                    // 		 Render channels with Vue.js
+                });
+			},
+			hasError(field) {
+				return Object.keys(this.errors).indexOf(field) >= 0;
+			}
+		}
 	}
 </script>
